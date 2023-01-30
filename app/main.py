0ceb942110209d2ce2c8ff7925f64dadd9e9d86d
Mark Corwin <mark@carkmorw.in>
2023-01-29 19:45:10 -0500

    Respond with pong

--- app/main.py
+++ app/main.py
@@ -9,7 +9,14 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    con, _ = server_socket.accept() # wait for client
+
+    while True:
+        _ = con.recv(1024)
+        con.send(bytes('+PONG\r\n', 'utf-8'))
+
+        # con.send(bytes("+OK\r\n", 'utf-8'))
+        # server_socket.close()
 
 
 if __name__ == "__main__":
